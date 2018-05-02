fc6a513e122557c974724dcac7fc32b0f946c1df
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-05-02 04:20:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,12 +4,11 @@ attr_accessor :name, :genre, :artist
 
 def initialize(name, genre)
   @name = name
-  @genre = genre 
+  @genre = genre
+  
+  
 end
 
- def add_song(song)
-   @songs << song 
- end
-
+ 
 
 end
