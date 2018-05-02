0de4301ae692bcc88f7520b6cc91fa2e54b60821
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-05-02 04:19:26 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -6,13 +6,13 @@ def initialize(name)
   @name = name 
   @songs = []
   
-  def add_song(song)
-   @songs << song 
- end
-end
+  
+
 end
 
- 
+ def add_song(song)
+   @songs << song 
+ end
   
   
 def songs 
